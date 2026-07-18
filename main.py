c004945cb02bb67c70320e789e5024625b84c956
Chengay Samdrup Wangchuk <samdrupchengay47@gmail.com>
2026-07-17 18:45:17 -0600

    view all problems and searching by title

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import json
 import os 
 def save_problems(problems):
     with open("problems.json", "w") as f:
-        json.dump(problems,f)
+        json.dump(problems, f, indent=4)
 
 def load_problems():
     if os.path.exists("problems.json"):
         with open("problems.json", "r") as f:
-            data = json.load(f)
-            return data
+            content = f.read()
+            if content.strip() == "":
+                return []
+            else:
+                data = json.loads(content)
+                return data
     else:
         return []
 def add_problems(title,difficulty,topic,date_solved):
@@ -37,22 +41,49 @@ def topic_breakdown():
     problems = load_problems()
     topic_counts  = {}
     for problem in problems:
-        topic = problem["topic"]
+        topic = problem["topic"].lower()
         if topic in topic_counts:
             topic_counts[topic]+=1
         else:
             topic_counts[topic]=1
     return topic_counts
 
+def view_all():
+    index = 1
+    problems = load_problems()
+    for problem in problems:
+        print(f"{index}.")
+        for key,value in problem.items():
+            print(f"{key}: {value}")
+        
+        print("")
+        index+=1
+def search(target):
+    problems = load_problems()
+    matches = []
+    for problem in problems:
+        if target.lower() in problem["title"].lower():
+            matches.append(problem)
+    return matches
+  
+    
+
+        
+
 
 
 def menu():
     while True:
+        print("")
+        print("*** LeetCode Tacker ***")
         print("""
-        1.Add a new problem
-        2.View Summary
-        3.View Topic Breakdown
-        4.Quit""")
+    1.Add a new problem
+    2.View Summary
+    3.View Topic Breakdown
+    4.View All Problems
+    5.Search by Title
+    6.Quit""")
+        print("")
 
         choice = input("Please enter your choice: ")
 
@@ -65,15 +96,37 @@ def menu():
             print("The problem has been added!")
         elif choice == "2":
             summaries = summary()
-            print(summaries)
+            print("=== Summary ===")
+            for key,value in summaries.items():
+                print(f"{key}: {value}")
         elif choice == "3":
             ans = topic_breakdown()
-            print(ans)
+            print("=== Topic Breakdown ===")
+            for key,value in ans.items():
+                print(f"{key}: {value}")
         elif choice =="4":
+            view_all()
+        elif choice =="5":
+            user_input= input("Please input the title of the problem : ")
+            ans = search(user_input)
+            match = False
+            for i,elem in enumerate(ans):
+                print(f"{i+1}.")
+                for key,value in elem.items():
+                    print(f"{key}: {value}")
+                    match = True
+                print("")
+            if not match:
+                print("No title found by that name, please try a vaild title")
+            
+        elif choice =="6":
             print("Thank you for using the tracker, Goodbye")
             break
         else:
             print("invalid input, please try again with a valid input")
-            
+
 
 menu()
+
+            
+
